dd73ec228784d9fe6a0324ba1a451f3bda3f35e2
noambrihun <noambrihun1@gmail.com>
2025-11-05 20:01:55 +0200

    add

--- uuid.js
+++ uuid.js
@@ -1,15 +1,17 @@
-const uuid = require('uuid');
+const {v4:uuid}= require('uuid');
 
 const students = [
-  { fullName: "Dana Levi", grade: 92 },
+  { fullName: "Dana   Levi", grade: 92 },
   { fullName: "Itay Cohen", grade: 81 },
 ]
-
+ 
 const idStudents = students.map((student,index) => {
   return { 
     ...student, 
-    id: index + 1
+    id: uuid()
  };
 });
 
 console.log(idStudents);
+
+
